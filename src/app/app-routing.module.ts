f2b7c50e54cf1ac574d60fd2431e3e195a455f40
Manuela <brunner.manu@hotmail.com>
2023-01-04 11:21:27 +0100

    remove empty lines and add spacings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about-me', component: AboutMeComponent},
-  {path: '**', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
