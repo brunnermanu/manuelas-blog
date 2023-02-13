ad755b15ab8edc7998e476b33b9aca6c27e4c58b
Manuela <brunner.manu@hotmail.com>
2023-02-13 06:08:57 +0100

    remove border-radius-map and unnecessary code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { Article } from "../../Interfaces/articleInterface";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  articles: Article[];
-}
+export class HomeComponent {}
